84b2ec6ed01b962e9e5cde4c31221249371d2250
Dingyu Peng <dpeng4@illinois.edu>
2019-04-28 21:50:14 -0500

    removed graphene

--- Automatic identification of graphene/gray_rgb.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-def g2r(imp):
-    w, h = im.shape
-    ret = np.empty((w, h, 3), dtype=np.uint8)
-    ret[:, :, 0] = im
-    ret[:, :, 1] = im
-    ret[:, :, 2] = im
-    return ret
